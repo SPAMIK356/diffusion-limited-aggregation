c26dd081b899ce93eb371e7b8f80ca474fadd1ce
SPAMIK356 <maximbondarenko20@gmail.com>
2025-07-10 17:35:52 +0300

    Added velocities

--- main.py
+++ main.py
@@ -18,17 +18,24 @@ running = True
 
 #Particles
 positions = np.random.random((config.PARTICLE_AMOUNT,2))*(config.WIDTH, config.HEIGHT)
+velocities = np.random.random(positions.shape)*config.SPEED_MULTIPLIER
 
 while running:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-
     screen.fill((255,255,255))
+    draw_circles(positions,config.RADIUS, config.COLOR, screen)
+
     pygame.display.flip()
 
-    clock.tick(config.FPS_LIMIT)
+    
+    deltaTime = clock.tick(config.FPS_LIMIT) / 1000.0
+
+    positions+= velocities*deltaTime
+    
 
 
 
