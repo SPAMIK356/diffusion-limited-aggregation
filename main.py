b919ed5670e7ad115989cf4268cde0b5d2247410
SPAMIK356 <maximbondarenko20@gmail.com>
2025-07-10 17:25:52 +0300

    Added circle drawing func

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import numpy as np
 import config
 
 
+def draw_circles(positions: np.typing.NDArray, radius: float, color: tuple, surface: Surface) -> None:
+    for x, y in positions:
+        pygame.draw.circle(surface, color, (x,y),radius)
+
+
 #Pygame initialisation
 pygame.init()
 screen = pygame.display.set_mode((config.WIDTH,config.HEIGHT))
@@ -11,13 +16,15 @@ pygame.display.set_caption(config.CAPTION)
 clock = pygame.time.Clock()
 running = True
 
+#Particles
+positions = np.random.random((config.PARTICLE_AMOUNT,2))*(config.WIDTH, config.HEIGHT)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-            
+
     screen.fill((255,255,255))
     pygame.display.flip()
 
