24059ebe78f7a52b8f647da5ad1d11a717217e4d
SPAMIK356 <maximbondarenko20@gmail.com>
2025-07-11 15:05:33 +0300

    Changed logic, function find neighbours doesn't work

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 import pygame
 from pygame import Surface
 import numpy as np
+from numpy.typing import NDArray
 import config
 
+def draw_grid(grid: NDArray, sim_surface: Surface, width, height) -> Surface:
+    '''Generate a surface with grid and scales it to width and height'''
+    pygame.surfarray.blit_array(sim_surface,grid)
+    scaled_surface = pygame.transform.scale(sim_surface, (width, height))
+    return scaled_surface
 
-def draw_circles(positions: np.typing.NDArray, radius: float, color: tuple, surface: Surface, render_not_stuck = True, is_stuck = None) -> None:
-    if render_not_stuck:
-        for x, y in positions:
-            pygame.draw.circle(surface, color, (x,y),radius)
-        return
-    for x, y in positions[is_stuck]:
-            pygame.draw.circle(surface, color, (x,y),radius)
-
-def wrap(positions: np.typing.NDArray, x_limit:float, y_limit:float, offset:float) -> None:
-    positions[positions[:,0]>x_limit+offset,0] = -offset
-    positions[positions[:,0]<-offset,0] = x_limit+offset
-    positions[positions[:,1]>y_limit+offset,1] = -offset
-    positions[positions[:,1]<-offset,1] = y_limit+offset
-
-def calculate_distance(positions: np.typing.NDArray, is_stuck: np.typing.NDArray) -> np.typing.NDArray:
-    stuck_pos = positions[is_stuck]
-    rel_positions = positions[:, np.newaxis,:]-stuck_pos[np.newaxis,:,:]
-    distances = np.sum(rel_positions**2, 2)
-    return np.sqrt(distances)
+def find_neighbours(is_stuck : NDArray, walkers : NDArray):
+    have_neigbours = np.full(walkers.shape[0], False)
+    for a in range(-1, 2):
+        for b in range(-1,2):
+            if a == 0 and b == 0:
+                continue
+            have_neigbours += is_stuck[walkers[:,0]+a, walkers[:,1]+b]
+    return have_neigbours
 
 #Pygame initialisation
 pygame.init()
@@ -32,36 +27,21 @@ clock = pygame.time.Clock()
 running = True
 
 #Particles
-positions = np.random.random((config.PARTICLE_AMOUNT,2))*(config.WIDTH, config.HEIGHT)
-velocities = np.random.random(positions.shape)*config.SPEED_MULTIPLIER
-is_stuck = np.full(config.PARTICLE_AMOUNT,False)
-is_stuck[0] = True
-render_not_stuck = config.RENDER_NOT_STUCK
+is_stuck = np.full((config.WIDTH, config.HEIGHT),False)
+is_stuck[int(config.WIDTH/2), int(config.HEIGHT/2)] = True
 while running:
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    walkers = np.random.randint(0, config.WIDTH, (config.PARTICLE_AMOUNT, 2))
+    print(find_neighbours(is_stuck, walkers))
     screen.fill((255,255,255))
-    draw_circles(positions,config.RADIUS, config.COLOR, screen, render_not_stuck, is_stuck)
+    #render here
 
     pygame.display.flip()
 
-    
-    deltaTime = clock.tick(config.FPS_LIMIT) / 1000.0
-
-    velocities[is_stuck] = 0
-    positions+= velocities*deltaTime
-    
-    velocities*= config.DAMPING
-    
-    distances = calculate_distance(positions, is_stuck)
-    closeness_mask = np.sum(distances<config.RADIUS*config.STUCK_MULTIPPLIER,1).astype(np.bool)
-
-    is_stuck[closeness_mask] = True
-
-    wrap(positions, config.WIDTH, config.HEIGHT, config.RADIUS)
     
 
 
